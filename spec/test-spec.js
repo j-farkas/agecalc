fdda10a6fe01805f917a42488c0d7979e2d3442c
Student <example@example.com>
2019-05-31 08:56:09 -0700

    Implement rest of planets

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -7,7 +7,7 @@ describe('Agecalc', function() {
 
      console.log(age);
      expect(age.GetEarthAge()).toEqual(23);
-     age = new Agecalc(new Date('May 31, 1995'));
+     age = new Agecalc(new Date('January 31, 1995'));
      expect(age.GetEarthAge()).toEqual(24);
   });
 
@@ -29,21 +29,21 @@ describe('Agecalc', function() {
     let age = new Agecalc(new Date('May 31, 1995'));
 
      console.log(age);
-     expect(age.GetMarsAge()).toEqual(12.7);
+     expect(age.GetMarsAge()).toEqual(12);
   });
 
   it('should output the users age on jupiter' , function() {
     let age = new Agecalc(new Date('May 31, 1995'));
 
      console.log(age);
-     expect(age.GetMarsAge()).toEqual(2);
+     expect(age.GetJupiterAge()).toEqual(2);
   });
 
   it('should output the users age on saturn' , function() {
     let age = new Agecalc(new Date('May 31, 1995'));
 
      console.log(age);
-     expect(age.GetMarsAge()).toEqual(0);
+     expect(age.GetSaturnAge()).toEqual(0);
   });
 
 
--- src/age.js
+++ src/age.js
@@ -5,17 +5,35 @@ export class Agecalc{
   }
 
   GetEarthAge(){
-    const earthYear = 365.25;
+    const year = 365.26;
     let days = Math.floor((new Date() - this.age) / this.msPerDay);
-
-    return Math.floor(days / earthYear);
+    return Math.floor(days / year);
   }
 
   GetMercuryAge(){
-    const mercYear = 88;
+    const year = 87.97;
     let days = Math.floor((new Date() - this.age) / this.msPerDay);
-
-    return Math.floor(days / mercYear);
+    return Math.floor(days / year);
+  }
+  GetVenusAge(){
+    const year = 224.7;
+    let days = Math.floor((new Date() - this.age) / this.msPerDay);
+    return Math.floor(days / year);
+  }
+  GetMarsAge(){
+    const year = 685.75;
+    let days = Math.floor((new Date() - this.age) / this.msPerDay);
+    return Math.floor(days / year);
+  }
+  GetJupiterAge(){
+    const year = 4332.59;
+    let days = Math.floor((new Date() - this.age) / this.msPerDay);
+    return Math.floor(days / year);
+  }
+  GetSaturnAge(){
+    const year = 10759;
+    let days = Math.floor((new Date() - this.age) / this.msPerDay);
+    return Math.floor(days / year);
   }
 }
 
