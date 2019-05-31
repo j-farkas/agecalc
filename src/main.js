fb05600cd7d2497cde04064e3e67030cd292d7d9
Student <example@example.com>
2019-05-31 10:13:25 -0700

    Prepare to draw grid

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-// import { Agecalc } from './age.js';
-// import $ from 'jquery';
-//
-// $(document).ready(function(){
-//   });
+//import { Agecalc } from './age.js';
+import $ from 'jquery';
+import '../css/styles.css';
+import 'bootstrap';
+
+$(document).ready(function(){
+  $(".container").append("<div class='grid-container'>");
+
+  for(let i = 0; i < 43; i++){
+  $(".grid-container").append("<div class='grid-item i'>"+i+"</div>");
+}
+$(".container").append("</div>");
+  });
