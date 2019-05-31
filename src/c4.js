47b0974cd574c2a5c93c7c194899f162ebbb3f6b
Student <example@example.com>
2019-05-31 13:21:28 -0700

    fix bug with checking sides

--- src/c4.js
+++ src/c4.js
@@ -43,78 +43,85 @@ export class Connect{
         winner[this.board[i]]++;
       }if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("left");
         return this.gameOver;
       }
     }
     if(possible.ul === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move-8; i > 0; i-=8)
+      for(let i = move-8; i > move-32; i-=8)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("upeft");
         return this.gameOver;
       }
     }
     if(possible.u === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move-7; i > 0; i-=7)
+      for(let i = move-7; i > move-21; i-=7)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("up");
         return this.gameOver;
       }
     }
     if(possible.ur === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move-6; i > 0; i-=6)
+      for(let i = move-6; i > move-24; i-=6)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("upright");
         return this.gameOver;
       }
     }
     if(possible.dr === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move+8; i <42; i+=8)
+      for(let i = move+8; i <move+32; i+=8)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("downright");
         return this.gameOver;
       }
     }
     if(possible.dl === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move+6; i <42; i+=6)
+      for(let i = move+6; i <move+24; i+=6)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("downleft");
         return this.gameOver;
       }
     }
     if(possible.r === 1){
       let winner = {}
       winner[this.board[move]] = 1;
-      for(let i = move+1; i <42; i++)
+      for(let i = move+1; i < move+4; i++)
       {
         winner[this.board[i]]++;
       }
       if(winner[this.board[move]] === 4){
         this.gameOver = true;
+        console.log("right");
         return this.gameOver;
       }
     }
