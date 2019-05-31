4be97980dce439d9bf52d78c947857256d1b52c9
Student <example@example.com>
2019-05-31 11:18:38 -0700

    Implement piece dropping to bottom

--- src/main.js
+++ src/main.js
@@ -7,17 +7,23 @@ let connect = new Connect();
 
 function attachListeners() {
   $(".container").on("click", ".grid-item", function(){
-    console.log($(this));
-    $(this).addClass(connect.getTurn());
-    connect.turn++;
+    let column = $(this).attr('id').slice(1);
+    console.log($("#a"+column)[0]);
+    for(let i = column % 7; i < 42; i+=7){
+      if($("#a"+i)[0].classList.contains("red") === false && $("#a"+i)[0].classList.contains("blue") === false){
+        $("#a"+i).addClass(connect.getTurn());
+        connect.turn++;
+        break;
+    }
+    }
   })
 }
 $(document).ready(function(){
   attachListeners();
   $(".container").append("<div class='grid-container'>");
 
-  for(let i = 41; i >= 0 ; i--){
-  $(".grid-container").append("<div class='grid-item' id='spot"+i+"'>◉</div>");
+  for(let i = 41; i >=0 ; i--){
+  $(".grid-container").append("<div class='grid-item' id='a"+i+"'>◉</div>");
 }
 $(".container").append("</div>");
   });
